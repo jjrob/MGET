702858b18e5f697fb094ff0846b53bdc8725f7d4
Jason J Roberts <jason.roberts@duke.edu>
2026-04-09 18:39:02 -0400

    Add GitHub Action to try to repro #54

--- src/GeoEco/R/PlumberAPI.R
+++ src/GeoEco/R/PlumberAPI.R
@@ -242,16 +242,6 @@ function(name, res) {
     for (line in lines) {
       cat("DEBUG:", line)
     }
-    
-    if ("data.frame" %in% class(value) || "tibble" %in% class(value)) {
-      colClasses <- vapply(value, function(x) paste(class(x), collapse="|"), FUN.VALUE=character(1))
-      cat(sprintf("DEBUG: GET serializer=feather rows=%d cols=%d names=%s\n",
-                  nrow(value), ncol(value), paste(names(value), collapse=",")))
-      cat(sprintf("DEBUG: GET feather column classes: %s\n",
-                  paste(sprintf("%s=%s", names(colClasses), colClasses), collapse=", ")))
-    } else {
-      cat(sprintf("DEBUG: GET serializer=json class=%s\n", paste(class(value), collapse="|")))
-    }
 
     # If the value is a dataframe or a tibble, set the serializer to feather.
     # Otherwise use unboxed JSON. Then return the value.
